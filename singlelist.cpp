a3b0c890b3a619230e3fe179b2e00e5efa9677f8
naufaalkh <naufaalkh@gmail.com>
2024-04-25 17:00:05 +0700

    case 1 untuk menambahkan data

--- singlelist.cpp
+++ singlelist.cpp
@@ -160,7 +160,15 @@ int main()
             cout << "5. Keluar" << endl;
             cout << "Pilihan: ";
             cin >> pilihan;
-            switch (pilihan)    
+            switch (pilihan)
+            {
+            case 1:
+                addNode();
+                cout << "Data Berhasil Ditambahkan" << endl;
+                system("pause");
+                system("cls");
+                break;
+            }
         }
         catch (exception e)
         {
