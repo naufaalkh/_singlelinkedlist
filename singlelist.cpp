237660be5a1c48f2cc9c6029b45e2cb63656684a
naufaalkh <naufaalkh@gmail.com>
2024-04-25 16:58:39 +0700

    membuat pilihan

--- singlelist.cpp
+++ singlelist.cpp
@@ -145,3 +145,27 @@ void searchData()
         cout << "Data tidak ditemukan" << endl;
     }
 }
+
+int main()
+{
+    int pilihan;
+    do
+    {
+        try
+        {
+            cout << "1. Tambah Data" << endl;
+            cout << "2. Hapus Data" << endl;
+            cout << "3.Tampilkan Data " << endl;
+            cout << "4. Cari Data " << endl;
+            cout << "5. Keluar" << endl;
+            cout << "Pilihan: ";
+            cin >> pilihan;
+            switch (pilihan)    
+        }
+        catch (exception e)
+        {
+            cout << "Terjadi kesalahan" << endl;
+        }
+
+    } while (pilihan != 5);
+}
